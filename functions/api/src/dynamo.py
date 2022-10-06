6e28e56280a4da6c3cfc740d4f01d39c8aa6f549
Michael Djorup <michael.djorup@gmail.com>
2022-10-06 13:54:59 -0400

    changed environment to be virtualenv

---
+++ functions/api/src/dynamo.py
@@ -0,0 +1,22 @@
+class DB:
+
+    def __init__(self, resource, table_name):
+
+        self.table = resource.Table(table_name)
+
+
+    
+    def put_item(self, item:dict):
+        """
+        Item = {
+            
+        }
+        
+        """
+        
+
+    def batch_put_item(self, items):
+        # TODO:
+        return 
+        
+
