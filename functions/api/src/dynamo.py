fe0f02541c15d0a2f31f7d44454b05c5e3b61d6c
Michael Djorup <michael.djorup@gmail.com>
2022-10-06 14:14:06 -0400

    planning for db writing

--- functions/api/src/dynamo.py
+++ functions/api/src/dynamo.py
@@ -3,20 +3,20 @@ class DB:
     def __init__(self, resource, table_name):
 
         self.table = resource.Table(table_name)
-
-
-    
-    def put_item(self, item:dict):
-        """
-        Item = {
-            
-        }
-        
-        """
         
 
-    def batch_put_item(self, items):
+    def upload_game(self, req_body):
         # TODO:
+
+        # This is a batch writer for dynamodb
+        # with self.table.batch_writer() as batch:
+        #     for _ in range(1000000):
+        #         batch.put_item(Item={'HashKey': '...',
+        #                             'Otherstuff': '...'})
+        #     # You can also delete_items in a batch.
+        #     batch.delete_item(Key={'HashKey': 'SomeHashKey'})
+
+        
         return 
         
 
